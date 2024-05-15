a9b82043e6a76436dc8638547fdf55bee2cea23c
RenanFumis <renan-oliveira19@hotmail.com>
2024-05-15 00:23:12 -0300

    criado dois arquivos na pasta mocks que carregam cada um, a resposta da requisição da API, dessa forma economizamos no valor de cada requisição e também no caso de se não tiver internet para fazer os testes

--- module01/aula02-stubs/src/service.test.js
+++ module01/aula02-stubs/src/service.test.js
@@ -5,12 +5,19 @@ const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
 
 //Service
 ;(async () => {
+// {
+
+//     //requisição feita externamente no caso internet
+//   const service = new Service();
+//   const dados = await service.makeRequest(BASE_URL_2)
+
+//   console.log('dados:', dados);
+// }
+
+const service = new Service();
+
 {
-  const service = new Service();
-  const dados = await service.makeRequest(BASE_URL_1)
-
-  console.log('dados:', dados);
+  
 }
-
 
 })();
