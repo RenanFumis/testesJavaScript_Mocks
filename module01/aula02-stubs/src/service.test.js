91a31d955a6583fae974d0d09860a1ca80f4c5c6
RenanFumis <renan-oliveira19@hotmail.com>
2024-05-15 00:29:03 -0300

    instalação da biblioteca sinon e inicio da preparação do ambiente isolado

--- module01/aula02-stubs/src/service.test.js
+++ module01/aula02-stubs/src/service.test.js
@@ -2,6 +2,9 @@ const Service = require('./service');
 const assert = require('assert');
 const BASE_URL_1 = "https://swapi.dev/api/planets/1/";
 const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
+//para chamar a biblioteca de sinon e criar um ambiente isolado
+const { createSandbox } = require('sinon');
+const sinon = createSandbox();
 
 //Service
 ;(async () => {
@@ -17,7 +20,7 @@ const BASE_URL_2 = "https://swapi.dev/api/planets/2/";
 const service = new Service();
 
 {
-  
+
 }
 
 })();
